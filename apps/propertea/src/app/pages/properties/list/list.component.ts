7fdec99e13e399d0fd391316560592d4ab4046c1
Maxime Ailloud <maxime.ailloud@gmail.com>
2024-06-28 16:29:29 +0200

    Add fake list with loading state

--- apps/propertea/src/app/pages/properties/list/list.component.ts
+++ apps/propertea/src/app/pages/properties/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SupabaseClient } from '@supabase/supabase-js';
 
@@ -6,14 +6,24 @@ import { SupabaseClient } from '@supabase/supabase-js';
     selector: 'propertea-properties-list',
     standalone: true,
     templateUrl: 'list.component.html',
+    styleUrl: 'list.component.scss',
     imports: [
         RouterLink,
     ]
 })
-export default class ListComponent implements OnInit {
+export default class ListComponent {
     private supabaseClient = inject(SupabaseClient);
 
-    public ngOnInit(): void {
-        console.log('list component');
+    public isLoading = true;
+    public properties = signal<{name: string; description: string}[]>([]);
+
+    public loadProperties(): void {
+        this.properties.set([
+            {
+                name: '1-5350 Avenue Bourbonniere',
+                description: '5350 Avenue Bourbonniere, H1X 2M9, Montreal QC, Canada'
+            }
+        ]);
+        this.isLoading = false;
     }
 }
--- apps/propertea/src/app/services/session.service.ts
+++ apps/propertea/src/app/services/session.service.ts
@@ -1,38 +1,46 @@
 import { inject, Injectable, signal, WritableSignal } from '@angular/core';
-import { AuthChangeEvent, AuthError, AuthOtpResponse, Session, SupabaseClient } from '@supabase/supabase-js';
+import {
+  AuthChangeEvent,
+  AuthError,
+  AuthOtpResponse,
+  Session,
+  SupabaseClient,
+} from '@supabase/supabase-js';
 import { SessionStore } from '@stores/session.store';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class SessionService {
-    private supabaseClient = inject(SupabaseClient);
-    private sessionStore = inject(SessionStore);
+  private supabaseClient = inject(SupabaseClient);
+  private sessionStore = inject(SessionStore);
 
-    public isSessionInitialised: WritableSignal<boolean> = signal(false);
+  public isSessionInitialised: WritableSignal<boolean> = signal(false);
 
-    public initialiseSession(): Promise<void> {
-        return this.supabaseClient.auth.getSession().then(({data}) => {
-            if (null !== data.session) {
-                this.isSessionInitialised.set(true);
-                this.sessionStore.setSession(data.session);
-            }
-        });
-    }
+  public initialiseSession(): Promise<void> {
+    return this.supabaseClient.auth.getSession().then(({ data }) => {
+      if (null !== data.session) {
+        this.isSessionInitialised.set(true);
+        this.sessionStore.setSession(data.session);
+      }
+    });
+  }
 
-    public monitorAuthChanges(): void {
-        this.supabaseClient.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
-            if (event === 'SIGNED_OUT') {
-                this.isSessionInitialised.set(false);
-            }
-        });
-    }
+  public monitorAuthChanges(): void {
+    this.supabaseClient.auth.onAuthStateChange(
+      (event: AuthChangeEvent, session: Session | null) => {
+        if (event === 'SIGNED_OUT') {
+          this.isSessionInitialised.set(false);
+        }
+      }
+    );
+  }
 
-    public signIn(email: string): Promise<AuthOtpResponse> {
-        return this.supabaseClient.auth.signInWithOtp({email});
-    }
+  public signIn(email: string): Promise<AuthOtpResponse> {
+    return this.supabaseClient.auth.signInWithOtp({ email });
+  }
 
-    public signOut(): Promise<{ error: AuthError | null }> {
-        return this.supabaseClient.auth.signOut();
-    }
+  public signOut(): Promise<{ error: AuthError | null }> {
+    return this.supabaseClient.auth.signOut();
+  }
 }
