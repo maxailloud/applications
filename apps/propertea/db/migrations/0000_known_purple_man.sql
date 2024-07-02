798c9a17ef55a4a3d8028e9c37601d0c7af6bf42
Maxime Ailloud <maxime.ailloud@gmail.com>
2024-07-02 16:56:43 +0200

    Update migrations with new schema

--- apps/propertea/db/migrations/0000_known_purple_man.sql
+++ apps/propertea/db/migrations/0000_known_purple_man.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS "property" (
-	"id" uuid PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text NOT NULL,
 	"address" text NOT NULL,
 	"user_id" uuid NOT NULL,
