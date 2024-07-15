2478254b215e5b10fb8db696c95342aa30faae67
Maxime Ailloud <maxime.ailloud@gmail.com>
2024-07-15 11:21:38 +0200

    Update build

--- dist/propertea/chunk-L4EXECXU.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c,d,e,g as f}from"./chunk-ZLYF62A5.js";import"./chunk-WSP4HVAI.js";f();export{c as Headers,d as Request,e as Response,b as default,a as fetch};
---
+++ dist/propertea/chunk-QCDOTGG3.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e,g as f}from"./chunk-HJUN6AIX.js";import"./chunk-LKWF3GQO.js";f();export{c as Headers,d as Request,e as Response,b as default,a as fetch};
