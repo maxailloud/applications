858128b37ce6e2482c5bf8b25d8c3d1c8c1dc31e
Maxime Ailloud <maxime.ailloud@gmail.com>
2024-07-16 14:30:23 +0200

    Add page 404 for github pages

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,4 @@
 PROJECT_NAME="$1"
 
 nx run build --prod --base-href https://maxailloud.github.io/applications/$PROJECT_NAME/ --project $PROJECT_NAME
+cp ./dist/$PROJECT_NAME/index.html ./dist/$PROJECT_NAME/404.html
