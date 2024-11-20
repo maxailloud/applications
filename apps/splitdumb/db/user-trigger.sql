33e42448e283972e0814109db88447d45d20bec8
Maxime Ailloud <maxime.ailloud@gmail.com>
2024-11-20 23:54:49 +0100

    Add trigger to handle update date automatically

--- apps/splitdumb/db/user-trigger.sql
+++ apps/splitdumb/db/user-trigger.sql
@@ -14,3 +14,12 @@ $$;
 create trigger on_auth_create_user_profile
     after insert on auth.users
     for each row execute procedure public.handle_new_user();
+
+-- Enable MODDATETIME extension
+create extension if not exists moddatetime schema extensions;
+
+-- This will set the `updated_at` column on every update
+create trigger handle_updated_at before update on groups
+  for each row execute procedure moddatetime (updated_at);
+create trigger handle_updated_at before update on expenses
+  for each row execute procedure moddatetime (updated_at);
